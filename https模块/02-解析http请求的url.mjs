31427ff649a25fe4c6dcfacea33959419e0f6971
nextc <1391040917@qq.com>
2023-10-08 20:55:32 +0800

    使用URL类获取请求路径和请求参数

--- https模块/02-解析http请求的url.mjs
+++ https模块/02-解析http请求的url.mjs
@@ -5,9 +5,17 @@ const server = http.createServer((req, res) => {
   // 获取请求的url
   console.log(req.url); // 只包含 url 中的路径和查询字符串
 
-  // 第二个参数为 true 时，会将 url 中的查询字符串解析为对象
-  const path_query = url.parse(req.url, true);
-  console.log(path_query);
+  //   // 方法1，已被弃用
+  //   // 第二个参数为 true 时，会将 url 中的查询字符串解析为对象
+  //   const path_query = url.parse(req.url, true);
+  //   console.log(path_query);
+
+  // 方法2
+  const myURL = new URL(req.url, "http://example.org");
+  console.log(myURL);
+  console.log(myURL.pathname); // 路径
+  const ids = myURL.searchParams.get("ids");
+  console.log(ids);
 
   // 此时，需要设置响应头
   res.setHeader("Content-Type", "text/html;charset=utf-8");
