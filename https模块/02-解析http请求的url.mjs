2177111ec6cc63a99ebb92d0dfd7e8296ba5b91f
nextc <1391040917@qq.com>
2023-10-08 20:22:09 +0800

    解析请求的url

---
+++ https模块/02-解析http请求的url.mjs
@@ -0,0 +1,21 @@
+import http from "http";
+import url from "url";
+
+const server = http.createServer((req, res) => {
+  // 获取请求的url
+  console.log(req.url); // 只包含 url 中的路径和查询字符串
+
+  // 第二个参数为 true 时，会将 url 中的查询字符串解析为对象
+  const path_query = url.parse(req.url, true);
+  console.log(path_query);
+
+  // 此时，需要设置响应头
+  res.setHeader("Content-Type", "text/html;charset=utf-8");
+  res.end("你好");
+});
+
+// 监听9000端口
+// http协议默认端口是80，https协议默认端口是443
+server.listen(9000, () => {
+  console.log("server is running at http://127.0.0.1:9000");
+});
